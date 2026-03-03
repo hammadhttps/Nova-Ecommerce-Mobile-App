1fc357faecbc050c619d121c27ee2b8e471ee5fd
hammadhttps <hammadulhassan2620@gmail.com>
2026-03-03 20:00:00 +0500

    Fix SafeAreaView deprecation + seed Firestore with 72 products

--- src/features/home-tabs/HomeFeedScreen.tsx
+++ src/features/home-tabs/HomeFeedScreen.tsx
@@ -1,13 +1,14 @@
 import React, { useCallback } from "react";
 import { useNavigation } from "@react-navigation/native";
 import type { NavigationProp } from "@react-navigation/native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import ForYouScreen from "@/features/home/ForYouScreen";
 import { RootStackParamList } from "@/navigation/types";
 import { Product } from "@/types";
 
 const HomeFeedScreen: React.FC = React.memo(function HomeFeedScreen() {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const insets = useSafeAreaInsets();
 
   const onProductPress = useCallback(
     (product: Product) => {
@@ -16,11 +17,7 @@ const HomeFeedScreen: React.FC = React.memo(function HomeFeedScreen() {
     [navigation],
   );
 
-  return (
-    <SafeAreaView style={{ flex: 1 }} edges={["left", "right", "bottom"]}>
-      <ForYouScreen onProductPress={onProductPress} />
-    </SafeAreaView>
-  );
+  return <ForYouScreen onProductPress={onProductPress} />;
 });
 
 export default HomeFeedScreen;
