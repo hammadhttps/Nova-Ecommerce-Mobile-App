b4cac09e6cca4f92817fa75617877f922f2f5c7e
hammadhttps <hammadulhassan2620@gmail.com>
2026-05-03 20:16:00 +0500

    Optimize FlatList performance on Search screens

--- src/features/search/CategoriesTab.tsx
+++ src/features/search/CategoriesTab.tsx
@@ -89,6 +89,9 @@ export default React.memo(function CategoriesTab({
         columnWrapperStyle={styles.grid}
         contentContainerStyle={styles.listContent}
         showsVerticalScrollIndicator={false}
+        windowSize={5}
+        maxToRenderPerBatch={10}
+        removeClippedSubviews
       />
     </View>
   );
